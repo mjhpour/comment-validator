ef093533706067cb3085796d19ced02dd732de3e
Mohammad Javad <mjavadhpour@gmail.com>
2018-10-11 14:29:59 +0330

    add comment to index.php

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ require __DIR__.'/vendor/autoload.php';
 
 $coreCommentValidator = new CoreCommentValidator(new ValidateApplicator());
 $comment = new CommentModel();
+// Change this string to test the app. you can use the component 
+// in any way you want. this is just a test.
 $comment->setText("Violated word 1 1234567");
 
 echo $coreCommentValidator->validate($comment) ? "Is Valid!\n" : "Violated!\n";
